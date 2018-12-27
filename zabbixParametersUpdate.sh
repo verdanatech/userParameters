6cac1cd01f68e8854fba7765e214a9a6e12564fb
Verdanatech Soluções em TI <halexsandro.sales@verdanatech.com>
2018-12-27 08:47:38 -0300

    Criação do arquivo
    criação do novo arquivo.

---
+++ zabbixParametersUpdate.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# -------------------------------------------------------------------------
+# @Programa 
+# 	@name: zabbixParametersUpdate.sh
+#	@versao: 1.0.0
+#	@Data 14 de Dezembro de 2018
+#	@Copyright: Verdanatech Soluções em TI, 2018, https://www.verdanatech.
+# --------------------------------------------------------------------------
+# LICENSE
+#
+# zabbixParametersUpdate.sh is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# zabbixParametersUpdate.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------
+
+cd /tmp
+
+rm -rf userParameters
+
+git clone https://github.com/verdanatech/userParameters.git
+
+chmod +x userParameters/*
+
+mv userParameters/*.sh /bin/
+
+
